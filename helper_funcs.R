c8e7a0ca2dc6b778670b429895ee174667aa8c99
hpink97 <64581037+hpink97@users.noreply.github.com>
2023-07-27 19:37:29 +0100

    more helper funcs

--- helper_funcs.R
+++ helper_funcs.R
@@ -11,7 +11,8 @@ allowed_fungi <- c(allowed_bot,allowed_sclero)
 
 
 get_genes_query <- function(input_genes){
-  valid_genes <- input_genes[grepl('Lsat_1_v5_gn_\\d',input_genes)]
+  valid_genes <- input_genes[grepl('Lsat_1_v5_gn_\\d|AT\\d[g|G]\\d+',input_genes)]
+  valid_genes <- unique(valid_genes)
   genes_query = paste0("('", paste(valid_genes, collapse = "', '"), "')")
 
   if(length(valid_genes)>=1){
@@ -72,10 +73,20 @@ get_divset_exp <- function(genes=c('Lsat_1_v5_gn_5_78841','Lsat_1_v5_gn_9_69201'
 
 }
 
-get_timeseries_exp <- function(genes=c('Lsat_1_v5_gn_5_78841','Lsat_1_v5_gn_9_69201','Lsat_1_v5_gn_1_56161'),
-                               fungi=c('Bc','Ss','lkjhjk'),
+
+calculate_confidence_interval <- function(row) {
+  selected_values <- row[!is.na(row)]
+  lower_bound <- quantile(selected_values, 0.025)
+  upper_bound <- quantile(selected_values, 0.975)
+  return(c(lower_bound, upper_bound))
+}
+
+get_timeseries_exp <- function(genes=c('Lsat_1_v5_gn_3_121961','Lsat_1_v5_gn_9_69201','invalid_id'),
+                               fungi=c('Bc','Ss','invalid_fungi'),
                                include_raw_points = TRUE,
-                               include_mock = TRUE){
+                               include_mock = TRUE,
+                               include_conf_intervals = TRUE,
+                               ts_DEGs_only =FALSE){
 
   fungi = tolower(fungi) %>% gsub(' ','',.) %>% gsub('\\.|_','',.)
   fungi <- ifelse(fungi[fungi %in% allowed_fungi] %in% allowed_sclero,
@@ -100,6 +111,9 @@ get_timeseries_exp <- function(genes=c('Lsat_1_v5_gn_5_78841','Lsat_1_v5_gn_9_69
   if(length(fungi) ==1){
     WHERE <- paste0(WHERE," AND Fungi = '",fungi, "'")
   }
+  if(ts_DEGs_only){
+    WHERE <- paste0(WHERE," AND GeneID IN (SELECT GeneID FROM timeseries_overlap_degs)")
+  }
 
   my_query = paste0(SELECT, " FROM timeseries ",WHERE)
 
@@ -107,19 +121,27 @@ get_timeseries_exp <- function(genes=c('Lsat_1_v5_gn_5_78841','Lsat_1_v5_gn_9_69
 
 
   ##add confidence intervals
+  if(include_conf_intervals){
+    confidence_intervals <- t(apply(exp[,c('rep1','rep2','rep3')], 1, calculate_confidence_interval))
+    exp$'LCL' <- confidence_intervals[,1]
+    exp$'UCL' <- confidence_intervals[,2]
 
 
 
+  }
+
+  return(exp)
 
 
 }
 
 
+get_timeseries_exp(ts_DEGs_only = TRUE)
+
 
 get_GO_genes <- function(go_id='GO:0009723',
                              subset_lettuce_ids=NULL,ts_degs_only = FALSE){
 
-  conn <- db_connect()
 
 
 
@@ -128,26 +150,29 @@ get_GO_genes <- function(go_id='GO:0009723',
                         columns=c("TAIR"),
                         keytype="GO")
 
+  if(nrow(At_genes_w_GO)<2){return(NULL)}
+
+
+  WHERE <- paste('WHERE AGI IN', get_genes_query(At_genes_w_GO$TAIR))
+
   if(ts_degs_only){
-    ts_degs <- dbGetQuery(conn= conn,statement = 'SELECT GeneID FROM timeseries_overlap_degs' )
-    query = paste0("SELECT GeneID FROM gene_annotations WHERE AGI IN ",
-                   paste0("('", paste(unique(At_genes_w_GO$TAIR), collapse = "', '"), "')"),
-                   'AND GeneID IN', paste0("('", paste(ts_degs$GeneID, collapse = "', '"), "')"))
+    WHERE <- paste0(WHERE, " AND GeneID IN (SELECT GeneID FROM timeseries_overlap_degs)")
 
-
-  }else if(is.null(subset_lettuce_ids)){
-    query = paste0("SELECT GeneID FROM gene_annotations WHERE AGI IN ",
-                   paste0("('", paste(unique(At_genes_w_GO$TAIR), collapse = "', '"), "')"))
-  }else{
-
-    query = paste0("SELECT GeneID FROM gene_annotations WHERE AGI IN ",
-                   paste0("('", paste(unique(At_genes_w_GO$TAIR), collapse = "', '"), "')"),
-                   'AND GeneID IN', paste0("('", paste(subset_lettuce_ids, collapse = "', '"), "')"))
-
+  }else if(!is.null(subset_lettuce_ids)){
+    let_genes_query <- get_genes_query(subset_lettuce_ids)
+    if(!is.null(let_genes_query)){
+      WHERE <- paste0(WHERE, " AND GeneID IN ", let_genes_query)
+    }
   }
-  Ls_genes_w_GO <- dbGetQuery(conn= conn,statement = query )
 
-  db_disconnect(conn)
+  query <- paste0("SELECT GeneID,",
+                  "CASE WHEN (At_ShortName IS NULL OR At_ShortName LIKE 'AT%G%') THEN GeneID ",
+                  "ELSE 'Ls' || At_ShortName END AS name ",
+                  "FROM gene_annotations ", WHERE)
+
+
+  Ls_genes_w_GO <- db_query(query)
+
 
 
 
@@ -158,38 +183,50 @@ get_GO_genes <- function(go_id='GO:0009723',
 }
 
 
-get_genes_w_domain <- function(domain_id='PF00067', domain_desc=NULL,
-                               subset_lettuce_ids=NULL,ts_degs_only = FALSE){
+get_genes_w_domain <- function(domain_id='PF00067',
+                               domain_desc=NULL,
+                               subset_lettuce_ids=NULL,
+                               ts_degs_only = FALSE,
+                               include_gene_name = TRUE){
 
-  conn <- db_connect()
 
   if( !is.null(domain_desc)){
-    domain_WHERE= paste(" LOWER(description) LIKE",
+    WHERE= paste("WHERE LOWER(da.description) LIKE",
                   sprintf("'%%%s%%'", tolower(domain_desc)))
   }else{
-    domain_WHERE = paste0(" id =", "'",domain_id,"'")
+      WHERE = paste0("WHERE da.id =", "'",domain_id,"'")
 
   }
 
   if(ts_degs_only){
-    ts_degs <- dbGetQuery(conn= conn,statement = 'SELECT GeneID FROM timeseries_overlap_degs' )
-    geneID_WHERE = paste0(' AND GeneID IN', paste0("('", paste(ts_degs$GeneID, collapse = "', '"), "')"))
+    WHERE <- paste0(WHERE, " AND da.GeneID IN (SELECT GeneID FROM timeseries_overlap_degs)")
   }else if(!is.null(subset_lettuce_ids)){
-    paste0(' AND GeneID IN', paste0("('", paste0(subset_lettuce_ids, collapse = "', '"), "')"))
+    let_genes_query <- get_genes_query(subset_lettuce_ids)
+    if(!is.null(let_genes_query)){
+      WHERE <- paste0(WHERE, " AND da.GeneID IN ", let_genes_query)
 
-  }else{
-    geneID_WHERE = ""
+    }
   }
 
-  query <- paste0("SELECT DISTINCT GeneID FROM domain_annotations WHERE", domain_WHERE, geneID_WHERE)
+  if(include_gene_name){
+    query <-paste("SELECT DISTINCT da.GeneID,",
+           "CASE WHEN (ga.At_ShortName IS NULL OR ga.At_ShortName LIKE 'AT%G%') THEN da.GeneID",
+           "ELSE 'Ls' || ga.At_ShortName END AS name",
+           "FROM domain_annotations AS da",
+           "LEFT JOIN gene_annotations AS ga ON da.GeneID = ga.GeneID",
+           WHERE)
 
-  #print(query)
+  }else{
+    query <- paste0("SELECT DISTINCT GeneID FROM domain_annotations AS da ", WHERE)
+    }
+
+  print(query)
 
 
 
-  Ls_genes_w_domain <- dbGetQuery(conn= conn,statement = query )
 
-  db_disconnect(conn)
+  Ls_genes_w_domain <- db_query(query)
+
 
   return(Ls_genes_w_domain)
 
@@ -200,7 +237,6 @@ get_orthologs <- function(AGI= 'AT3G10500', shortname=NULL,
                           subset_lettuce_ids=NULL,ts_degs_only = FALSE){
 
 
-  conn <- db_connect()
 
   if(!is.null(shortname)){
 
@@ -224,12 +260,11 @@ get_orthologs <- function(AGI= 'AT3G10500', shortname=NULL,
   query <- paste0("SELECT GeneID FROM gene_annotations WHERE AGI ='",
                   AGI,"'")
 
-  Ls_genes_w_domain <- dbGetQuery(conn= conn,statement = query )
+  Ls_genes_w_domain <- db_query(query)
 
 
 
 
-  db_disconnect(conn)
   return(Ls_genes_w_domain)
 
 
@@ -239,7 +274,7 @@ get_orthologs <- function(AGI= 'AT3G10500', shortname=NULL,
 
 
 # get_GO_orthologs(go_id = 'GO:0009723', ts_degs_only=TRUE)
-# test <- get_genes_w_domain(domain_desc = 'ethylene', ts_degs_only=FALSE)
+#test <- get_genes_w_domain(domain_desc = 'AP2', ts_degs_only=TRUE)
 #get_orthologs(shortname = 'LBD29')
 
 
